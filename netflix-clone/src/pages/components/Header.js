424b1ad76223498be061cd753125f96d2d9ece95
Ranja Func <seolin9340@gmail.com>
2021-04-03 18:11:24 +0900

    Header 컴포넌트 로고 이미지 src 수정

--- netflix-clone/src/pages/components/Header.js
+++ netflix-clone/src/pages/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="header">
             <h1>
                 <span className="hide">Raflix</span>                
-                <img src="/images/Devices.png" alt="" />               
+                <img src="./images/Netflix_Logo_PMS2.png" alt="로고" />               
             </h1>    
             <div className="login">
                 <Link to='/login'>로그인</Link>   
